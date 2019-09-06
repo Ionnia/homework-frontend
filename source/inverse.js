dba0082a202e4d8aa4012c6fd692f55f6cb3c645
Valentine Baymakanov <baymakanov@gmail.com>
2019-09-06 09:04:50 +0300

    Рефакторинг

--- source/inverse.js
+++ source/inverse.js
@@ -1,3 +1,11 @@
 'use strict';
 
-const inverse = (array, number = 0) => Array.isArray(array) && Number.isInteger(number) ? number < 0 ? [...array.slice(0, number).reverse(), ...array.slice(number)] : [...array.slice(0, number), ...array.slice(number).reverse()] : 'Invalid argument(s)';
+const validate = (array, number) => Array.isArray(array) && Number.isInteger(number);
+const inverse = (array, number = 0) => {
+    if(!validate(array, number)){
+        return 'Invalid argument(s)';
+    }
+    const sortingPart = array.slice(number);
+    const addingPart = array.slice(0, number);
+    return number < 0 ? [...addingPart.reverse(), ...sortingPart] : [...addingPart, ...sortingPart.reverse()];
+}
